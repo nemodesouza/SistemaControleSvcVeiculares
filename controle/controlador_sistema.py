588c28aee68ffa61773f1288ef6f54c56ce535a3
NEMO DE SOUZA <>
2021-04-03 17:42:22 +0000

    Nomes das pastas em minúsculas, para evitar problemas no coding. e implementação do código em controlador_sistema incorretamente comitado na branch do luís.

---
+++ controle/controlador_sistema.py
@@ -0,0 +1,32 @@
+from limite.tela_sistema import TelaSistema
+from controle.controlador_veiculo import ControladorVeiculo
+from controle.controlador_cliente import ControladorCliente
+from controle.controlador_relatorio import ControladorRelatorio
+from controle.controlador_revisao import ControladorRevisao 
+
+
+
+class ControladorSistema:
+    def __init__(self):
+        self.__tela_sistema = TelaSistema()
+
+    def inicializa_sistema(self):
+        
+        print("INICIALIZANDO SISTEMA")
+        
+        self.abre_tela()
+
+    def controlador_veiculos(self):
+        print (">> CONTROLADOR DE VEICULOS SELECIONADO -- EBA!!!")
+    
+    def abre_tela(self):
+        print(">>Abrindo tela")
+        lista_opcoes = {1: self.controlador_veiculos}
+
+        while True:
+
+            opcao_escolhida =       self.__tela_sistema.tela_opcoes()
+
+            servico_escolhido = lista_opcoes[opcao_escolhida]
+
+            servico_escolhido()
