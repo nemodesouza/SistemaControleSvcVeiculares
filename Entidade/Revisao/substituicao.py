9fc7a0ad285503172e9f6f6414b3bf247bb70a51
silveiraluisf <>
2021-04-03 00:33:11 +0000

    Acrescentei os objetos de classe substituicao e verificacao

--- Entidade/Revisao/substituicao.py
+++ Entidade/Revisao/substituicao.py
@@ -5,9 +5,32 @@ class Substituicao():
         self.__descricao = descricao
 
     @property
-    def identificador(self) -> int:
+    def identificador(self):
         return self.__identificador
 
     @property
-    def descricao(self) -> str:
+    def descricao(self):
         return self.__descricao 
+
+
+I = Substituicao(1, "Substituir óleo do motor")
+
+II = Substituicao(2, "Substituir o filtro de óleo do motor")
+
+III = Substituicao(3, "Substituir o filtro de combustível")
+
+IV = Substituicao(4, "Substituir o anel de vedação do bujão do cárter do motor")
+
+V = Substituicao(5, "Substituir a anilha do mecanismo do pneu de estepe")
+
+VI = Substituicao(6, "Substituir as velas de ignição")
+
+VII = Substituicao(7, "Substituir o filtro de ar do motor")
+
+VII = Substituicao(8, "Substituir o fluido de freio")
+
+IX = Substituicao(9, "Substituir a correia dentada")
+
+X = Substituicao(10, "Substituir a correia auxiliar")
+
+XI = Substituicao(11, "Substituir o líquido de arrefecimento")
--- Entidade/Revisao/verificacao.py
+++ Entidade/Revisao/verificacao.py
@@ -11,3 +11,25 @@ class Verificacao():
     @property
     def descricao(self) -> str:
         return self.__descricao 
+
+I = Verificacao(1, "Verificar nível do óleo do motor")
+
+II = Verificacao(2, "Verificar o filtro de óleo do motor")
+
+III = Verificacao(3, "Verificar o filtro de combustível")
+
+IV = Verificacao(4, "Verificar o anel de vedação do bujão do cárter do motor")
+
+V = Verificacao(5, "Verificar a anilha do mecanismo do pneu de estepe")
+
+VI = Verificacao(6, "Verificar as velas de ignição")
+
+VII = Verificacao(7, "Verificar o filtro de ar do motor")
+
+VII = Verificacao(8, "Verificar o fluido de freio")
+
+IX = Verificacao(9, "Verificar a correia dentada")
+
+X = Verificacao(10, "Verificar a correia auxiliar")
+
+XI = Verificacao(11, "Verificar o líquido de arrefecimento")
